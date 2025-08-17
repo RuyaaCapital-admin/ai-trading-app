e47b455f99b237aafbea2f40e29d1f27d59120d8
ruyaa <admin@ruyaacapital.com>
2025-08-17 12:10:36 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@modelcontextprotocol/sdk'],
-  },
-  env: {
-    EODHD_API_KEY: process.env.EODHD_API_KEY,
-  }
-};
-
-export default nextConfig;
+const nextConfig = {};
+module.exports = nextConfig;
